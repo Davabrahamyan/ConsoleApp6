f6d20a5c07518a5d7cc9e031f9b35009fe8b52ba
vahra <vahra@DESKTOP-SO0L1VA>
2022-07-25 16:31:40 +0300

    Add project files.

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public  class Class1
+    {
+        public string[]arr=new string[6];
+        
+        public bool Method()
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            foreach(string i in arr)
+            {                
+                    //for(int j=0;j<6;j++)
+                    //{
+                    //if (i[j]==)
+                    //}                
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int a = rnd.Next(100000,200000)/ 100;
+        int b = 22 * a;
+        Console.WriteLine(rnd);
+        Console.WriteLine(b);
+    }
+
+}
+
+
